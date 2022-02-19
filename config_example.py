261049398141e52dea5b9eb54de00b35e6e4087a
Christian Perez <64086398+Christian0510@users.noreply.github.com>
2022-02-18 19:29:10 -0500

    Update config_example.py

--- config_example.py
+++ config_example.py
@@ -1,3 +1,4 @@
+import os
 import json
 
 # Solo si se prefiere usar un JSON en lugar de un archivo de configuracion de python
@@ -7,8 +8,8 @@ def get_data(key):
 
 
 class Config(object):
-    TOKEN = get_data('token')
+    TOKEN = get_data('token') or os.getenv('BOT_TOKEN')
 
-    CHANNEL_ID = get_data('channel')
+    CHANNEL_ID = get_data('channel') or os.getenv('CHANNEL_ID')
 
     SUDO_USERS = get_data('sudo')
