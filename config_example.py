c5bb921577dfb60a8860ff6d48a036d4b0afed1f
Christian Perez <64086398+Christian0510@users.noreply.github.com>
2022-02-18 20:09:02 -0500

    Update config_example.py

--- config_example.py
+++ config_example.py
@@ -8,8 +8,8 @@ def get_data(key):
 
 
 class Config(object):
-    TOKEN = get_data('token') or os.getenv('BOT_TOKEN')
+    TOKEN = os.getenv('BOT_TOKEN')
 
-    CHANNEL_ID = get_data('channel') or os.getenv('CHANNEL_ID')
+    CHANNEL_ID = os.getenv('CHANNEL_ID')
 
     SUDO_USERS = get_data('sudo')
